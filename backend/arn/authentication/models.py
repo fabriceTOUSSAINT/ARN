ee174f7499231d6d8a3cca1e886d1324bc32b8b4
Fabrice Toussaint <fabricetoussaint@Fabrices-MacBook-Pro.local>
2020-11-14 14:54:53 -0500

    update

--- backend/arn/authentication/models.py
+++ backend/arn/authentication/models.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class ArnUser(AbstractUser):
+    # Idk, not using any of these fields yet really
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True, blank=True)
     email = models.CharField(max_length=120)
-    first_name = models.CharField(max_length=120, blank=True)
-    last_name = models.CharField(max_length=120, blank=True)
